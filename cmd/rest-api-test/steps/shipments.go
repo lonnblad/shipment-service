f668d13a88e2379d77312a0a31aaf9f9c91aef12
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2021-05-06 12:18:30 +0200

    Lowered linting thresholds (#2)

--- cmd/rest-api-test/steps/shipments.go
+++ cmd/rest-api-test/steps/shipments.go
@@ -1,6 +1,10 @@
 package steps
 
 import (
+	"fmt"
+	"strconv"
+
+	"github.com/cucumber/godog"
 	v1 "github.com/lonnblad/shipment-service-backend/boundaries/rest/v1"
 )
 
@@ -20,4 +24,40 @@ func newCreateShipmentRequest() v1.CreateShipmentRequest {
 	req.Package.Weight = 10
 
 	return req
+}
+
+func decorateWithValues(createShipmentReq v1.CreateShipmentRequest, values *godog.Table) (_ v1.CreateShipmentRequest, err error) {
+	for _, row := range values.Rows {
+		key := row.Cells[0].Value
+		value := row.Cells[1].Value
+
+		switch key {
+		case "sender - name":
+			createShipmentReq.Sender.Name = value
+		case "sender - email":
+			createShipmentReq.Sender.Email = value
+		case "sender - address":
+			createShipmentReq.Sender.Address = value
+		case "sender - country code":
+			createShipmentReq.Sender.CountryCode = value
+		case "receiver - name":
+			createShipmentReq.Receiver.Name = value
+		case "receiver - email":
+			createShipmentReq.Receiver.Email = value
+		case "receiver - address":
+			createShipmentReq.Receiver.Address = value
+		case "receiver - country code":
+			createShipmentReq.Receiver.CountryCode = value
+		case "package - weight":
+			createShipmentReq.Package.Weight, err = strconv.Atoi(value)
+			if err != nil {
+				return
+			}
+		default:
+			err = fmt.Errorf("unsupported key: %s", key)
+			return
+		}
+	}
+
+	return createShipmentReq, nil
 }
